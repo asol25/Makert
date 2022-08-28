5f59e7db6054096fd2c11f6b48b18ad38c1cdeb8
asol25 <usool.203@gmail.com>
2022-08-28 10:20:16 +0700

    business login wallet

--- src/Wallet.java
+++ src/Wallet.java
@@ -30,15 +30,36 @@ public class Wallet {
     }
 
     /**
-     *
-     * @param dollars Amount of dollars
-     * @return
+     * Adds dollars to the wallet.
+     * @param dollars Given amount of the dollars.
      */
-    public boolean checkWithDrawDollars(double dollars) {
-        return this.dollars < dollars ? false : true;
+    public void givenDollars(double dollars) {
+        this.dollars += dollars;
     }
 
+    /**
+     * Adds coins to the wallet.
+     * @param coins Given amount of the coins.
+     */
+    public void givenCoins(double coins) {
+        this.coins += coins;
+    }
+
+    /**
+     * Check the amount of dollars is affordable.
+     * @param dollars Amount of the dollars of the wallet.
+     * @return TRUE if the given amount of dollars is affordable, FALSE otherwise.
+     */
+    public boolean checkWithDrawDollars(double dollars) {
+        return this.dollars <= dollars;
+    }
+
+    /**
+     * Check the amount of coins is affordable.
+     * @param coins Amount of the coins of the wallet.
+     * @return TRUE if the given amount of coins is affordable, FALSE otherwise.
+     */
     public boolean checkWithDrawCoins(double coins) {
-        return  this.coins < coins ? false : true;
+        return  this.coins <= coins;
     }
 }
