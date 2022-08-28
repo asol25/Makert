5f66c203dff92b95333ac64a6138794bdbc7ad04
asol25 <usool.203@gmail.com>
2022-08-28 10:31:22 +0700

    flow business logic of wallet

--- src/Wallet.java
+++ src/Wallet.java
@@ -31,19 +31,45 @@ public class Wallet {
 
     /**
      * Adds dollars to the wallet.
-     * @param dollars Given amount of the dollars.
+     * @param dollars The given amount of the dollars.
      */
-    public void givenDollars(double dollars) {
+    public void depositDollars(double dollars) {
         this.dollars += dollars;
     }
 
     /**
      * Adds coins to the wallet.
-     * @param coins Given amount of the coins.
+     * @param coins The given amount of the coins.
      */
-    public void givenCoins(double coins) {
+    public void depositCoins(double coins) {
         this.coins += coins;
     }
+
+    /**
+     * Add dollars to blocked dollars.
+     * @param dollars The given amount of the dollars.
+     */
+    public void blockedDollars(double dollars) {
+        this.dollars -= dollars;
+        this.blockedDollars += dollars;
+    }
+
+    /**
+     * Add coins to blocked coins.
+     * @param coins The given amount of the coins.
+     */
+    public void blockedCoins(double coins) {
+        this.coins -= coins;
+        this.blockedCoins += coins;
+    }
+
+    /**
+	 * Pays the given amount of dollars from the blocked dollars.
+	 * @param dollars The given amount of dollars.
+	 */
+	public void payFromBlockedDollars(double dollars) {
+		blockedDollars -= dollars;
+	}
 
     /**
      * Check the amount of dollars is affordable.
@@ -53,13 +79,39 @@ public class Wallet {
     public boolean checkWithDrawDollars(double dollars) {
         return this.dollars <= dollars;
     }
-
+    
+	/**
+	 * Pays the given amount of coins from the blocked coins.
+	 * @param coins The given amount of coins.
+	 */
+	public void payFromBlockedCoins(double coins) {
+		blockedCoins -= coins;
+	}
+	
     /**
      * Check the amount of coins is affordable.
      * @param coins Amount of the coins of the wallet.
      * @return TRUE if the given amount of coins is affordable, FALSE otherwise.
      */
     public boolean checkWithDrawCoins(double coins) {
-        return  this.coins <= coins;
+        return this.coins <= coins;
     }
+
+    /**
+	 * Checks if the given amount of dollars can be withdrawn from the blocked dollars.
+	 * @param dollars The given amount of dollars.
+	 * @return TRUE if the given amount of dollars is affordable, FALSE otherwise.
+	 */
+	public boolean checkBlockedDollars(double dollars) {
+		return dollars <= blockedDollars;
+	}
+    
+	/**
+	 * Checks if the given amount of coins can be withdrawn from the blocked coins.
+	 * @param coins The given amount of coins.
+	 * @return TRUE if the given amount of coins is affordable, FALSE otherwise.
+	 */
+	public boolean checkBlockedCoins(double coins) {
+		return coins <= blockedCoins;
+	}
 }
